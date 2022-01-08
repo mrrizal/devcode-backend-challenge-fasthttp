7f331cf35a4cd2da0ea54c34eda679d369795420
rizal <rizal@lomotif.com>
2022-01-08 23:08:50 +0700

    feat: split function for get activity group

--- handler/activity_groups.go
+++ handler/activity_groups.go
@@ -43,32 +43,26 @@ func CreateActivityGroups(ctx *fasthttp.RequestCtx) {
 		utils.GenerateResponse("Success", "Success", activityGroup))
 }
 
-func GetActivityGroups(ctx *fasthttp.RequestCtx) {
-	path := strings.TrimSuffix(string(ctx.Path()), "/")
-	splitedPath := strings.Split(path, "/")
-
-	// get activity group by id
-	if len(splitedPath) == 3 {
-		activityGroupID, err := strconv.Atoi(splitedPath[len(splitedPath)-1])
-		if err != nil {
-			utils.ResponseHandler(ctx, fasthttp.StatusNotFound,
-				utils.GenerateResponse("Not Found", fmt.Sprintf("Activity with ID %s Not Found",
-					splitedPath[len(splitedPath)-1]), nil))
-			return
-		}
-		activityGroup := new(models.ActivityGroup)
-		if err := activityGroup.GetByID(activityGroupID, activityGroup); err != nil {
-			utils.ResponseHandler(ctx, fasthttp.StatusNotFound,
-				utils.GenerateResponse("Not Found", fmt.Sprintf("Activity with ID %d Not Found",
-					activityGroupID), nil))
-			return
-		}
-		utils.ResponseHandler(ctx, fasthttp.StatusOK,
-			utils.GenerateResponse("Success", "Success", activityGroup))
+func GetActivityGroupsByID(ctx *fasthttp.RequestCtx, splitedPath []string) {
+	activityGroupID, err := strconv.Atoi(splitedPath[len(splitedPath)-1])
+	if err != nil {
+		utils.ResponseHandler(ctx, fasthttp.StatusNotFound,
+			utils.GenerateResponse("Not Found", fmt.Sprintf("Activity with ID %s Not Found",
+				splitedPath[len(splitedPath)-1]), nil))
 		return
 	}
+	activityGroup := new(models.ActivityGroup)
+	if err := activityGroup.GetByID(activityGroupID, activityGroup); err != nil {
+		utils.ResponseHandler(ctx, fasthttp.StatusNotFound,
+			utils.GenerateResponse("Not Found", fmt.Sprintf("Activity with ID %d Not Found",
+				activityGroupID), nil))
+		return
+	}
+	utils.ResponseHandler(ctx, fasthttp.StatusOK,
+		utils.GenerateResponse("Success", "Success", activityGroup))
+}
 
-	// get all activity group
+func GetAllActivityGroups(ctx *fasthttp.RequestCtx) {
 	activityGroups := []models.ActivityGroup{}
 
 	activityGroup := new(models.ActivityGroup)
@@ -79,6 +73,20 @@ func GetActivityGroups(ctx *fasthttp.RequestCtx) {
 	}
 	utils.ResponseHandler(ctx, fasthttp.StatusOK,
 		utils.GenerateResponse("Success", "Success", activityGroups))
+}
+
+func GetActivityGroups(ctx *fasthttp.RequestCtx) {
+	path := strings.TrimSuffix(string(ctx.Path()), "/")
+	splitedPath := strings.Split(path, "/")
+
+	// get activity group by id
+	if len(splitedPath) == 3 {
+		GetActivityGroupsByID(ctx, splitedPath)
+		return
+	}
+
+	// get all activity group
+	GetAllActivityGroups(ctx)
 }
 
 func PatchActivityGroups(ctx *fasthttp.RequestCtx) {
